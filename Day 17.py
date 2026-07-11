74cfb77798bdda131233570423d54574317000a0
Jaswanthande <138223430+Jaswanthande@users.noreply.github.com>
2026-07-11 12:37:24 +0530

    Add files via upload

---
+++ Day 17.py
@@ -0,0 +1,99 @@
+"""
+MODULES
+A MODULE IS A PYTHON FILE (.py) THAT CONTAINS REUSABLE CODE
+1.VARIABLES
+2.FUNCTIONS
+3.CLASSES
+4.OBJECTS
+5.STATEMENTS
+
+WHY THIS
+INSTEAD OF WRITING THE SAME CODE REPEATEDLY WE CAN STORE IT IN A MODULE AND USE IT WHENEVER NEEDED
+
+TYPES OF MODULES
+1.USER DEFINE
+
+2.BUILT IN
+  math
+  os      THE OS MODULE IS USED TO INTERACT WITH OPERATING SYSTEM
+  sys     THIS WILL PROVIDE THE INFORMATION ABOUT PYTHON INTERPETER
+  random  USED TO GENERATE RANDOM VALUES
+
+
+IMPORT SPECIFIC FUNCTION
+
+EXAMPLES
+import ex
+print(ex.add(45,7))
+print(ex.sub(75,25))
+
+
+SPECIFIC FUNCTIONS
+from ex import add,sub
+print(add(1,2))
+
+FOR ALL ACCESS
+from ex import *
+print(add(1,2))
+
+
+EXAMPLE OF MATH
+import math
+print(math.sqrt(25))
+print(math.factorial(5))
+print(math.pow(2,5))
+print(math.pi)
+
+
+sqrt()        SQUARE ROOT
+factorial()   FACTORIAL
+pow()         POWER
+pi()          PI VALUE
+
+
+EXAMPLE OF OS
+import os
+print(os.getcwd())
+os.mkdir("JASWANTH042005")
+os.rmdir("JASWANTH042005")
+
+getcwd() CURRENT DIRECTORY
+mkdir()  CREATE FOLDER
+rmdir()  REMOVE FOLDER
+
+
+EXAMPLE OF SYS
+import sys
+print(sys.version)
+
+
+EXAMPLE OF RANDOM
+import random
+print(random.randint(1000,9999))
+
+import random
+print(random.randint(1000,9999))
+color = ['YELLOW','RED','BLUE','GREEN']
+print(random.choice(color))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
