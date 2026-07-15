6a3d2dc5631ddabb792561dbf1aded44cbd729a6
Jaswanthande <138223430+Jaswanthande@users.noreply.github.com>
2026-07-15 13:08:26 +0530

    Add files via upload

---
+++ Day20.py
@@ -0,0 +1,55 @@
+"""
+OOPS
+code reusable
+easy manintainece
+clear understanding
+better security
+
+CLASSES
+class is a bluprint or template used to create an object
+
+class batch_4:
+    pass
+
+OBJECT
+object is a instance of the class
+
+EXAMPLE:
+class student:
+    student ="JASWANTH"
+s1 = student()
+print(s1)
+
+class student:
+    student ="JASWANTH"
+    print(student)
+s1=student()
+
+ATTRIBUTE
+attributes are the varaiable that belongs to an object or the class
+
+EXAMPLE:
+class how:
+    def details(self,name,age):
+        self.name=name
+        self.age=age
+
+s1=how()
+s1.details("JASWANTH",21)
+print(s1.name)
+print(s1.age)
+
+METHODS
+Methods are nothing but the function inside the class
+EXAMPLE
+class calculator:
+    def add(self,num,num_2):
+        print(num + num_2)
+    def sub(self,num,num_2):
+        print(num-num_2)
+cal = calculator()
+cal.add(34,6)
+cal.sub(34,4)
+
+"""
+
